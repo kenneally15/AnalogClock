fc51fbbe61db1bbea556b3e8fbfa59e22b0592a0
kenneally15 <kenneally15@yahoo.com>
2025-04-01 19:52:13 -0400

    Node JS server

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 function updateClock() {
+    // Get current time in EST
     const now = new Date();
+    const estOffset = -4; // EDT offset (UTC-4)
+    const utc = now.getTime() + (now.getTimezoneOffset() * 60000);
+    const estDate = new Date(utc + (3600000 * estOffset));
     
     // Calculate degrees for each hand
-    const secondsDegrees = ((now.getSeconds() / 60) * 360) + 90;
-    const minutesDegrees = ((now.getMinutes() / 60) * 360) + ((now.getSeconds() / 60) * 6) + 90;
-    const hoursDegrees = ((now.getHours() / 12) * 360) + ((now.getMinutes() / 60) * 30) + 90;
+    const secondsDegrees = ((estDate.getSeconds() / 60) * 360) + 90;
+    const minutesDegrees = ((estDate.getMinutes() / 60) * 360) + ((estDate.getSeconds() / 60) * 6) + 90;
+    const hoursDegrees = ((estDate.getHours() / 12) * 360) + ((estDate.getMinutes() / 60) * 30) + 90;
     
     // Update hand rotations
     document.querySelector('.second-hand').style.transform = `rotate(${secondsDegrees}deg)`;
